e4aaecad526a5e2fb1e550395b661b57a3432080
syncrostone <jasminestone@gmail.com>
2018-10-05 14:53:33 -0400

    remove unneeded setup args

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ from distutils.core import setup
 
 setup(
     name='Sisyphus',
-    version='0.4dev',
+    version='0.5dev',
     packages=['sisyphus2'],
     license='MIT',
     long_description=open('README.md').read(),
 
-    install_requires=['docutils>=0.3', 'tensorflow', 'numpy'],
-    python_requires='>=2.7,!=3.0.*,!=3.1.*',
     author="Alex Atanasov, David Brandfonbrener, Daniel Ehrlich, Jasmine Stone",
     author_email="daniel.ehrlich@yale.edu",
     description="Easy-to-use package for the modeling and analysis of neural network dynamics, directed towards cognitive neuroscientists.",
