aef77f997898ae03c1569a90be04e0d3ba5fc377
syncrostone <jasminestone@gmail.com>
2018-10-05 10:50:00 -0400

    setup filled out

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Sisyphus',
     version='0.1dev',
-    packages=['tasks','backend',],
+    packages=['tasks','backend','weights'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
+
+    install_requires=['docutils>=0.3', 'tensorflow', 'numpy']
+
+    author="Alex Atanasov, David Brandfonbrener, Daniel Ehrlich, Jasmine Stone",
+    author_email="daniel.ehrlich@yale.edu",
+    description="Easy-to-use package for the modeling and analysis of neural network dynamics, directed towards cognitive neuroscientists.",
+    license="MIT",
+    keywords="neuroscience, modeling, analysis, neural networks",
+    url="https://github.com/dbehrlich/sisyphus2/tree/networks-branch",
+
 )
