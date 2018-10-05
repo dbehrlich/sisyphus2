9d9efbfaaa6c8817c542e85ad51d20bf3ed1537b
syncrostone <jasminestone@gmail.com>
2018-10-05 11:50:20 -0400

    broader python requires

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='Sisyphus',
-    version='0.3dev',
+    version='0.4dev',
     packages=find_packages(),
     license='MIT',
     long_description=open('README.md').read(),
 
     install_requires=['docutils>=0.3', 'tensorflow', 'numpy'],
-    python_requires='==2.7',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*',
     author="Alex Atanasov, David Brandfonbrener, Daniel Ehrlich, Jasmine Stone",
     author_email="daniel.ehrlich@yale.edu",
     description="Easy-to-use package for the modeling and analysis of neural network dynamics, directed towards cognitive neuroscientists.",
